362f96d860259abcd5f551df26e76788b7459f7d
fidelscodes <fidelscodes@gmail.com>
2016-10-31 11:08:55 -0400

    Done

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,18 @@
+require 'csv'
 class CustomersController < ApplicationController
 
   def index
     @customers = Customer.all
+  end
+
+  # handle file upload
+  def upload
+    # headers true signals that the first row just defines the fields and isn't actually part of the dataset
+    # each row is ordinal, so pos 0 = email, pos 1 = first_name, and pos 2 = last_name
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email:lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to customers_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
+
+  # route to take the file updload
+  post 'customers/upload', to: 'customers#upload'
 end
